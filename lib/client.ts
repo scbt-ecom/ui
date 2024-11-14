e2559ea20502a253ac8646da35397a6b738190d5
galeevri <galeevri@sovcombank.ru>
2024-11-14 14:27:58 +0300

    feat: 🎸 rich editor, some fix, fix pipe

--- lib/client.ts
+++ lib/client.ts
@@ -6,7 +6,7 @@ export * from './shared/ui/formControlElements'
 export * from './shared/ui/TabsSwitcher'
 export * from './shared/ui/modal'
 export * from './shared/ui/providers'
-export * from './shared/ui/popover'
+export * from './shared/ui/Popover'
 
 // Hooks
 export * from './shared/hooks'
