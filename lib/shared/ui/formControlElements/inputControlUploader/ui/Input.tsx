45d984f953e8b98e3dedba7a512f6c5ee372b1ec
zarutskiins <zarutskiins@sovcombank.ru>
2024-11-27 11:29:25 +0300

    fix: 🐛 absolute path bg styles uploader

--- lib/shared/ui/formControlElements/inputControlUploader/ui/Input.tsx
+++ lib/shared/ui/formControlElements/inputControlUploader/ui/Input.tsx
@@ -21,10 +21,10 @@ export const Input = forwardRef(function Input<T extends FieldValues>(
   return (
     <div
       className={cn(
-        'flex h-[64px] w-[476px] rounded-sm bg-[length:100%_100%] bg-no-repeat hover:border hover:border-primary-hover hover:bg-color-primary-tr-hover focus-visible:border focus-visible:border-primary-hover focus-visible:bg-color-primary-tr-hover focus-visible:outline-none active:border active:border-primary-hover active:bg-color-primary-tr-pressed [&:not(:hover)(:focus-visible)(:active)(:disabled)]:bg-[url("../../static/files/border.svg")]',
+        'flex h-[64px] w-[476px] rounded-sm bg-[length:100%_100%] bg-no-repeat hover:border hover:border-primary-hover hover:bg-color-primary-tr-hover focus-visible:border focus-visible:border-primary-hover focus-visible:bg-color-primary-tr-hover focus-visible:outline-none active:border active:border-primary-hover active:bg-color-primary-tr-pressed [&:not(:hover)(:focus-visible)(:active)(:disabled)]:bg-[url("/static/files/border.svg")]',
         classes?.borderContent,
         { 'bg-color-primary-light-default opacity-50': disabled },
-        { '[&:not(:hover)(:focus-visible)(:active)(:disabled)]:bg-[url("../../static/files/borderError.svg")]': error }
+        { '[&:not(:hover)(:focus-visible)(:active)(:disabled)]:bg-[url("/static/files/borderError.svg")]': error }
       )}
       ref={ref}
       {...dropzoneProps}
