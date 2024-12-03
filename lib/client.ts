9fd41efa48574803c1dd54129d6ed8826e4023ca
zarutskiins <zarutskiins@sovcombank.ru>
2024-12-03 12:52:15 +0300

    fix: after testing and client.ts

--- lib/client.ts
+++ lib/client.ts
@@ -3,7 +3,7 @@ import './shared/style.css'
 import './shared/utils-tailwind.css'
 // Components
 export * from './shared/ui/accordion'
-// export * from './shared/ui/formControlElements'
+export * from './shared/ui/formElements'
 export * from './shared/ui/TabsSwitcher'
 export * from './shared/ui/modal'
 export * from './shared/ui/providers'
