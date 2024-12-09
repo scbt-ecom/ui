598dd1ddb6c197b06f3403d586ede8ca8ad935cc
zarutskiins <zarutskiins@sovcombank.ru>
2024-12-09 15:07:22 +0300

    feat: 🎸 add to benefit component classes remove --quilet storyb

--- lib/widgets/benefit/Benefit.tsx
+++ lib/widgets/benefit/Benefit.tsx
@@ -2,18 +2,25 @@ import { type ReactElement } from 'react'
 import { type Content } from './model/types'
 import { BenefitItem } from './ui/BenefitItem'
 import { Heading, ResponsiveContainer } from '$/shared/ui'
+import { cn } from '$/shared/utils'
+
+export type IBenefitClasses = {
+  item?: string
+  heading?: string
+  grid?: string
+  container?: string
+  section?: string
+}
 
 export interface IBenefitProps {
   heading: string | ReactElement
   content: Content[]
-  classes?: {
-    item?: string
-  }
+  classes?: IBenefitClasses
 }
 
 export const Benefit = ({ heading, content, classes }: IBenefitProps) => {
   return (
-    <section className='pb-20'>
+    <section className={cn('pb-20', classes?.section)}>
       <ResponsiveContainer>
         <Heading className='mb-8' as='h2'>
           {heading}
--- lib/widgets/benefit/ui/BenefitItem.tsx
+++ lib/widgets/benefit/ui/BenefitItem.tsx
@@ -3,14 +3,23 @@ import { useDevice } from '$/shared/hooks'
 import { Button, Heading } from '$/shared/ui'
 import { cn, scrollToElement } from '$/shared/utils'
 
+export type IBenefitItemClasses = {
+  button?: string
+  item?: string
+  itemWrapper?: string
+  itemTextContainer?: string
+  itemDescription?: string
+  img?: string
+  imgWrapper?: string
+  headingBenefitItem?: string
+}
+
 interface IBenefitItemProps {
   title: string
   description: string | ReactElement
   img?: string
   mobileImg?: boolean
-  classes?: {
-    item?: string
-  }
+  classes?: IBenefitItemClasses
   index: number
 }
 
@@ -20,7 +29,12 @@ export const BenefitItem = ({ description, title, img, mobileImg, classes, index
   const button = (
     <>
       {index === 0 && isDesktop && (
-        <Button intent='primary' className='desktop:w-[200px]' onClick={() => scrollToElement('personal_form')} size='lg'>
+        <Button
+          intent='primary'
+          className={cn('desktop:w-[200px]', classes?.button)}
+          onClick={() => scrollToElement('personal_form')}
+          size='lg'
+        >
           Оформить заявку
         </Button>
       )}
@@ -36,19 +50,21 @@ export const BenefitItem = ({ description, title, img, mobileImg, classes, index
         classes?.item
       )}
     >
-      <div className='flex flex-col items-start justify-between px-4 desktop:py-8 desktop:pl-8'>
-        <div className='flex flex-col gap-4'>
-          <Heading as='h4' className='desktop:desk-title-bold-s'>
+      <div className={cn('flex flex-col items-start justify-between px-4 desktop:py-8 desktop:pl-8', classes?.itemWrapper)}>
+        <div className={cn('flex flex-col gap-4', classes?.itemTextContainer)}>
+          <Heading as='h4' className={cn('desktop:desk-title-bold-s', classes?.headingBenefitItem)}>
             {title}
           </Heading>
-          <p className='mob-body-regular-l text-icon-blue-grey-800 desktop:desk-body-regular-l'>{description}</p>
+          <p className={cn('mob-body-regular-l text-icon-blue-grey-800 desktop:desk-body-regular-l', classes?.itemDescription)}>
+            {description}
+          </p>
         </div>
         {button}
       </div>
 
       {(mobileImg || isDesktop) && (
-        <div className='w-full'>
-          <img className='h-[246px] object-cover' src={img} alt={title} />
+        <div className={cn('w-full', classes?.imgWrapper)}>
+          <img className={cn('h-[246px] object-cover', classes?.img)} src={img} alt={title} />
         </div>
       )}
     </li>
