ddcd4bc45ffa4b03f06e1801a0877d786dc9645c
muhametyanovti <muhametyanovti@sovcombank.ru>
2024-12-12 18:23:09 +0300

    refactor: refactor checkbox

---
+++ lib/shared/ui/checkbox/Checkbox.tsx
@@ -0,0 +1,45 @@
+import { forwardRef } from 'react'
+import { type CheckboxProps as CheckboxPrimitiveProps, Indicator, Root } from '@radix-ui/react-checkbox'
+import { Icon } from '$/shared/ui'
+import { cn } from '$/shared/utils'
+
+type CheckboxBaseClasses = {
+  root?: string
+  indicator?: string
+}
+
+export type CheckboxBaseProps = CheckboxPrimitiveProps & {
+  classes?: CheckboxBaseClasses
+  invalid?: boolean
+}
+
+export const CheckboxBase = forwardRef<HTMLButtonElement, CheckboxBaseProps>(
+  ({ classes, className, invalid, disabled, ...props }, ref) => {
+    return (
+      <Root
+        {...props}
+        disabled={disabled}
+        ref={ref}
+        className={cn(
+          'flex h-6 max-h-6 w-6 max-w-6 items-center justify-center rounded-full',
+          'relative rounded-sm border-2 border-blue-grey-700 outline-none',
+          'after:left-1/2 after:top-1/2 after:h-10 after:w-10 after:-translate-x-1/2 after:-translate-y-1/2 after:content-[""]',
+          'after:absolute after:rounded-full [&:not(:disabled)]:hover:after:bg-color-primary-tr-hover',
+          'after:duration-100 [&:not(:disabled)]:active:after:bg-color-primary-tr-pressed',
+          'after:-z-10 [&:not(:disabled)]:focus:after:bg-color-primary-tr-focus',
+          {
+            'border-negative': invalid,
+            'disabled:border-0 disabled:bg-color-blue-grey-300': disabled
+          },
+          classes?.root,
+          className
+        )}
+      >
+        <Indicator className='h-6 w-6 rounded-sm bg-color-primary-default group-disabled:bg-color-primary-disabled'>
+          <Icon name='general/check' className='size-6 text-icon-white' />
+        </Indicator>
+      </Root>
+    )
+  }
+)
+CheckboxBase.displayName = 'CheckboxBase'
