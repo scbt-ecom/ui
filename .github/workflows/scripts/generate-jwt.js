ff5b4adef4c36e391f2168641804a322288400b5
canfus <canfus69@gmail.com>
2025-01-24 02:52:27 +0300

    fix: add install keygen step

---
+++ .github/workflows/scripts/generate-jwt.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const { createAppAuth } = require('@octokit/auth-app')
+
+const appId = process.env.SEMANTIC_APP_ID;
+const privateKey = process.env.SEMANTIC_APP_PRIVATE_KEY;
+const clientId = process.env.SEMANTIC_APP_CLIENT_ID;
+const clientSecret = process.env.SEMANTIC_APP_CLIENT_SECRET;
+
+const auth = createAppAuth({
+  appId,
+  privateKey,
+  clientId,
+  clientSecret
+});
+
+auth()
+  .then(({ token }) => {
+  fs.writeFileSync('../jwt.txt', token);
+})
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
