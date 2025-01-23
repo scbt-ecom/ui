37d1fe84ba486624a6f4c347c3fd67a2f67ecccb
canfus <canfus69@gmail.com>
2025-01-23 12:40:01 +0300

    fix: remove unused fields from schema

--- src/stories/primitives/formElements/controlled/maskInput/schemas.ts
+++ src/stories/primitives/formElements/controlled/maskInput/schemas.ts
@@ -4,19 +4,7 @@ import { ZodUtils, zodValidators } from '$/shared/validation'
 export const baseSchema = z.object({
   field: zodValidators.base.getStringSchema({
     min: 3
-  }),
-  optional: zodValidators.base.getStringSchema({ required: false }),
-  object: z.object({
-    innerString: zodValidators.base.getStringSchema(),
-    innerNumber: zodValidators.base.getNumberSchema(),
-    innerNumberOptional: zodValidators.base.getNumberSchema({ required: false }),
-    innerObject: z.object({
-      email: zodValidators.base.getEmailSchema(),
-      name: zodValidators.base.getStringSchema()
-    }),
-    union: zodValidators.base.getUnionSchema(['A', 'B'] as const)
-  }),
-  array: z.array(zodValidators.base.getStringSchema())
+  })
 })
 export const baseDefaultValues = ZodUtils.getZodDefaults(baseSchema)
 
