e12f532d415713008c1e4a26de549c265a9a186f
muhametyanovti <muhametyanovti@sovcombank.ru>
2025-01-24 12:13:48 +0300

    help me

--- .github/workflows/scripts/generate-jwt.cjs
+++ .github/workflows/scripts/generate-jwt.cjs
@@ -1,25 +1,23 @@
-const fs = import('fs');
-const authApp = import('@octokit/auth-app');
+const fs = await import('fs');
+const authApp = await import('@octokit/auth-app');
 
 const appId = process.env.SEMANTIC_APP_ID;
 const privateKey = process.env.SEMANTIC_APP_PRIVATE_KEY;
 const clientId = process.env.SEMANTIC_APP_CLIENT_ID;
 const clientSecret = process.env.SEMANTIC_APP_CLIENT_SECRET;
 
-Promise.all([fs, authApp]).then(([fs, { createAppAuth }]) => {
-  const auth = createAppAuth({
-    appId,
-    privateKey,
-    clientId,
-    clientSecret
-  });
-
-  auth()
-    .then(({ token }) => {
-      fs.writeFileSync('../jwt.txt', token);
-    })
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
+const auth = authApp.createAppAuth({
+  appId,
+  privateKey,
+  clientId,
+  clientSecret
 });
+
+auth()
+  .then(({ token }) => {
+    fs.writeFileSync('../jwt.txt', token);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
