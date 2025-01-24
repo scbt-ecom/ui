693b7bab815eaa90384231736b5f6544d95a5ecc
muhametyanovti <muhametyanovti@sovcombank.ru>
2025-01-24 11:18:09 +0300

    fu

--- lib/widgets/stepper/ui/SingleStep.tsx
+++ lib/widgets/stepper/ui/SingleStep.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import parse from 'html-react-parser'
 import type { SingleStepProps } from '../model/types'
 import { Heading } from '$/shared/ui'
