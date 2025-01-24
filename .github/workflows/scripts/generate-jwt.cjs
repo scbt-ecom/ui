aa9f1286f106b51a29c5b122bae57794350e2091
muhametyanovti <muhametyanovti@sovcombank.ru>
2025-01-24 12:16:23 +0300

    help me

--- .github/workflows/scripts/generate-jwt.cjs
+++ .github/workflows/scripts/generate-jwt.cjs
@@ -1,23 +1,27 @@
-const fs = await import('fs');
-const authApp = await import('@octokit/auth-app');
+async function generateJwt() {
+  const fs = await import('fs');
+  const authApp = await import('@octokit/auth-app');
 
-const appId = process.env.SEMANTIC_APP_ID;
-const privateKey = process.env.SEMANTIC_APP_PRIVATE_KEY;
-const clientId = process.env.SEMANTIC_APP_CLIENT_ID;
-const clientSecret = process.env.SEMANTIC_APP_CLIENT_SECRET;
+  const appId = process.env.SEMANTIC_APP_ID;
+  const privateKey = process.env.SEMANTIC_APP_PRIVATE_KEY;
+  const clientId = process.env.SEMANTIC_APP_CLIENT_ID;
+  const clientSecret = process.env.SEMANTIC_APP_CLIENT_SECRET;
 
-const auth = authApp.createAppAuth({
-  appId,
-  privateKey,
-  clientId,
-  clientSecret
-});
-
-auth()
-  .then(({ token }) => {
-    fs.writeFileSync('../jwt.txt', token);
-  })
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
+  const auth = authApp.createAppAuth({
+    appId,
+    privateKey,
+    clientId,
+    clientSecret
   });
+
+  auth()
+    .then(({ token }) => {
+      fs.writeFileSync('../jwt.txt', token);
+    })
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
+
+generateJwt();
