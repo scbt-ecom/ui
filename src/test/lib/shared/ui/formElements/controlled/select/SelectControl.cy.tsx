d0c18698f41f38a4c8f584dac442c3fe24b60242
muhametyanovti <muhametyanovti@sovcombank.ru>
2025-01-29 17:28:54 +0300

    fix: 🐛 fix expression to close menu list

--- src/test/lib/shared/ui/formElements/controlled/select/SelectControl.cy.tsx
+++ src/test/lib/shared/ui/formElements/controlled/select/SelectControl.cy.tsx
@@ -56,7 +56,7 @@ describe('Test cases for Controlled.SelectControl', () => {
         cy.get('[data-test-id="select-item-0"]').click()
         cy.get('[data-test-id="select-item-1"]').click()
         // close list after values are selected
-        cy.get('span[id^=headlessui-combobox-button-]').click()
+        cy.get('body').click(0, 0)
       })
     // submit form
     cy.get('@submit').click()
