19dc1703265b860a39609b8c8f4cef087695806d
muhametyanovti <muhametyanovti@sovcombank.ru>
2025-01-29 18:05:38 +0300

    fix: 🐛 add comment

--- cypress/support/customCommands/clickOutside.ts
+++ cypress/support/customCommands/clickOutside.ts
@@ -1,3 +1,8 @@
+/**
+ * Функция для клика на элемент `body`
+ * @param x координата по оси x, `default: 0`
+ * @param y координата по оси y, `default: 0`
+ */
 export const clickOutside = (x: number = 0, y: number = 0) => {
   return cy.get('body').click(x, y)
 }
