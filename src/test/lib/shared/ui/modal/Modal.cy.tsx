2eabdfda9ccc3c5e90673b71a864e14b8042bbf0
canfus <canfus69@gmail.com>
2025-01-30 16:54:27 +0300

    fix: 🐛 fix modal test cases

--- src/test/lib/shared/ui/modal/Modal.cy.tsx
+++ src/test/lib/shared/ui/modal/Modal.cy.tsx
@@ -1,83 +1,26 @@
-import { useState } from 'react'
-import { ComponentFactory } from '@/test/utils'
+import { Component } from './BaseComponent'
+import { ComponentFactory } from '@/test/utils/ComponentFactory'
 import { Modal } from '$/shared/ui'
 
 describe('Test cases for Modal', () => {
+  const factory = new ComponentFactory(Modal)
+
   it('Should render and open by click', () => {
-    const factory = new ComponentFactory(Modal)
-
-    const Component = () => {
-      const [open, setOpen] = useState<boolean>(false)
-
-      return (
-        <>
-          {factory.getComponentProvider({
-            args: {
-              isModalOpen: open,
-              closeModal: () => setOpen(!open),
-              children: <p>Я модалка</p>,
-              title: 'Привет'
-            },
-            decorator: (Component, props) => (
-              <>
-                <button data-test-id='button' onClick={() => setOpen(true)}>
-                  Открыть модалку
-                </button>
-                <Component {...props} />
-              </>
-            )
-          })}
-        </>
-      )
-    }
-
-    cy.mount(<Component />)
+    cy.mount(<Component factory={factory} />)
 
     cy.get('[data-test-id="button"]').as('button')
 
-    cy.get('@button')
-      .click()
-      .then(() => {
-        cy.get('[data-test-id="modal"]').as('modal')
-        cy.get('@modal').should('be.visible')
-      })
+    cy.get('@button').click()
+    cy.get('[data-test-id="modal"]').as('modal')
+    cy.get('@modal', { timeout: 5000 }).should('exist').and('be.visible')
   })
 
   it('Should be default open and close by click on overlay', () => {
-    const factory = new ComponentFactory(Modal)
-
-    const Component = () => {
-      const [open, setOpen] = useState<boolean>(true)
-
-      return (
-        <>
-          {factory.getComponentProvider({
-            args: {
-              isModalOpen: open,
-              closeModal: () => setOpen(!open),
-              children: <p>Я модалка</p>,
-              title: 'Привет'
-            },
-            decorator: (Component, props) => (
-              <>
-                <button data-test-id='button' onClick={() => setOpen(true)}>
-                  Открыть модалку
-                </button>
-                <Component {...props} />
-              </>
-            )
-          })}
-        </>
-      )
-    }
-    cy.mount(<Component />)
+    cy.mount(<Component factory={factory} defaultOpen />)
 
     cy.get('[data-test-id="modal-overlay"]').as('modal-overlay')
 
-    cy.get('@modal-overlay', { timeout: 5000 })
-      .click('left')
-      .then(() => {
-        cy.get('[data-test-id="modal"]').should('not.be.visible')
-      })
+    cy.get('@modal-overlay').click('left')
+    cy.get('[data-test-id="modal"]', { timeout: 5000 }).should('not.exist')
   })
 })
