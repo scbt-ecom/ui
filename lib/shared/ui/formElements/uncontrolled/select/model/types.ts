39013f3fbdb1518ca9757219da3fdcfcf994854a
muhametyanovti <muhametyanovti@sovcombank.ru>
2025-02-26 15:47:35 +0300

    fix: fix select bug with attachment icon

--- lib/shared/ui/formElements/uncontrolled/select/model/types.ts
+++ lib/shared/ui/formElements/uncontrolled/select/model/types.ts
@@ -1,4 +1,5 @@
 import type { DeepPartial } from '$/shared/types'
+import { type AllowedIcons } from '$/shared/ui'
 import { type FieldAttachment } from '$/shared/ui/formElements/ui'
 
 type FieldAttachmentProps = React.ComponentPropsWithoutRef<typeof FieldAttachment>
@@ -9,7 +10,7 @@ export type SelectItemOption = {
   helperText?: string
   disabled?: boolean
   attachment?: {
-    left?: DeepPartial<FieldAttachmentProps>
+    left?: AllowedIcons
     right?: DeepPartial<FieldAttachmentProps>
   }
 }
