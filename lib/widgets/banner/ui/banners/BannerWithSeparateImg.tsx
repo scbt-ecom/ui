f8961df43deb2457e62a8b62a5ae85fe435fe70d
zarutskiins <zarutskiins@sovcombank.ru>
2025-05-29 16:05:55 +0300

    fix: banner advantages fix jsx

--- lib/widgets/banner/ui/banners/BannerWithSeparateImg.tsx
+++ lib/widgets/banner/ui/banners/BannerWithSeparateImg.tsx
@@ -23,90 +23,83 @@ export const BannerWithSeparateImg = ({
   }
 
   return (
-    <>
-      <section
-        id={widgetIds.banner}
-        data-test-id={widgetIds.banner}
+    <section id={widgetIds.banner} data-test-id={widgetIds.banner} className={cn('', classes?.root)}>
+      <ResponsiveContainer
         style={{ backgroundColor: color ?? '#d9edff' }}
-        className={cn('bg-banner-skyblue-300 desktop:h-[456px]', classes?.root)}
+        className={cn('relative bg-banner-skyblue-300 desktop:h-[456px]', classes?.container)}
       >
-        <ResponsiveContainer className={cn('relative', classes?.container)}>
-          <div className={cn('flex flex-col desktop:flex-row desktop:justify-between', classes?.wrapper)}>
-            <div className='flex flex-col desktop:gap-10 desktop:pt-20'>
-              <div className={cn('flex flex-col gap-4 pt-6 desktop:pt-0', classes?.textBlock)}>
-                <div
-                  dangerouslySetInnerHTML={{ __html: headTitle }}
-                  className={cn('mob-headline-bold-m text-color-dark desktop:desk-headline-bold-l', classes?.title)}
-                />
-                <div
-                  dangerouslySetInnerHTML={{ __html: subtitle }}
-                  className={cn('desk-body-regular-l text-color-dark', classes?.subtitle)}
-                />
-              </div>
-
+        <div className={cn('flex flex-col desktop:flex-row desktop:justify-between', classes?.wrapper)}>
+          <div className='flex flex-col desktop:gap-10 desktop:pt-20'>
+            <div className={cn('flex flex-col gap-4 pt-6 desktop:pt-0', classes?.textBlock)}>
               <div
-                className={cn(
-                  'grid-buttons-apply absolute bottom-6 left-1/2 z-10 flex w-full -translate-x-1/2 flex-col justify-self-center px-4 desktop:static desktop:left-auto desktop:max-w-full desktop:translate-x-0 desktop:flex-row desktop:justify-normal desktop:px-0',
-                  { 'flex items-center gap-4': secondary?.enabled },
-                  classes?.group
-                )}
-              >
-                {primary.enabled && (
-                  <ButtonWithHandlers
-                    className={cn('w-full desktop:max-w-[216px]', classes?.primary)}
-                    size='lg'
-                    intent='primary'
-                    {...primary.buttonContent}
-                  />
-                )}
-                {secondary?.enabled && (
-                  <ButtonWithHandlers
-                    intent='secondary'
-                    className={cn('w-full desktop:max-w-[216px]', classes?.secondary)}
-                    {...primary.buttonContent}
-                  />
-                )}
-              </div>
+                dangerouslySetInnerHTML={{ __html: headTitle }}
+                className={cn('mob-headline-bold-m text-color-dark desktop:desk-headline-bold-l', classes?.title)}
+              />
+              <div
+                dangerouslySetInnerHTML={{ __html: subtitle }}
+                className={cn('desk-body-regular-l text-color-dark', classes?.subtitle)}
+              />
             </div>
 
             <div
               className={cn(
-                'relative flex h-[356px] justify-center desktop:h-[456px] desktop:w-[550px]',
-                classes?.imageContainer
+                'grid-buttons-apply absolute bottom-6 left-1/2 z-10 flex w-full -translate-x-1/2 flex-col justify-self-center px-4 desktop:static desktop:left-auto desktop:max-w-full desktop:translate-x-0 desktop:flex-row desktop:justify-normal desktop:px-0',
+                { 'flex items-center gap-4': secondary?.enabled },
+                classes?.group
               )}
             >
-              {isMobile
-                ? image({ src: imgMobile.src, alt: imgAlt, className: cn('h-full object-contain') })
-                : image({
-                    src: imgDesktop.src,
-                    alt: imgAlt,
-                    className: cn('h-full object-contain')
-                  })}
+              {primary.enabled && (
+                <ButtonWithHandlers
+                  className={cn('w-full desktop:max-w-[216px]', classes?.primary)}
+                  size='lg'
+                  intent='primary'
+                  {...primary.buttonContent}
+                />
+              )}
+              {secondary?.enabled && (
+                <ButtonWithHandlers
+                  intent='secondary'
+                  className={cn('w-full desktop:max-w-[216px]', classes?.secondary)}
+                  {...primary.buttonContent}
+                />
+              )}
             </div>
           </div>
 
-          {advantages?.enabled && (
-            <div
-              className={cn(
-                'items-center justify-center rounded-md p-4 pb-8 mobile:hidden desktop:absolute desktop:bottom-[-50px] desktop:right-1/2 desktop:flex desktop:translate-x-1/2 desktop:p-0',
-                classes?.advantageContainer
-              )}
-            >
-              {advantages?.details && advantages?.details?.items.length > 0 && <Advantages {...advantages} />}
-            </div>
-          )}
-        </ResponsiveContainer>
-      </section>
+          <div
+            className={cn('relative flex h-[356px] justify-center desktop:h-[456px] desktop:w-[550px]', classes?.imageContainer)}
+          >
+            {isMobile
+              ? image({ src: imgMobile.src, alt: imgAlt, className: cn('h-full object-contain') })
+              : image({
+                  src: imgDesktop.src,
+                  alt: imgAlt,
+                  className: cn('h-full object-contain')
+                })}
+          </div>
+        </div>
+
+        {advantages?.enabled && (
+          <div
+            className={cn(
+              'items-center justify-center rounded-md p-4 pb-8 mobile:hidden desktop:absolute desktop:bottom-[-50px] desktop:right-1/2 desktop:flex desktop:translate-x-1/2 desktop:p-0',
+              classes?.advantageContainer
+            )}
+          >
+            {advantages?.details && advantages?.details?.items.length > 0 && <Advantages {...advantages} />}
+          </div>
+        )}
+      </ResponsiveContainer>
       {advantages?.enabled && (
         <div
           className={cn(
-            'flex items-center justify-center rounded-md p-4 pb-8 desktop:absolute desktop:bottom-[-50px] desktop:right-1/2 desktop:hidden desktop:translate-x-1/2 desktop:p-0',
+            'flex items-center justify-center rounded-md px-4 pb-8 pt-4 desktop:absolute desktop:bottom-[-50px] desktop:right-1/2 desktop:hidden desktop:translate-x-1/2 desktop:p-0',
             classes?.advantageContainer
           )}
         >
           {advantages?.details && advantages?.details?.items.length > 0 && <Advantages {...advantages} />}
         </div>
       )}
-    </>
+    </section>
   )
 }
