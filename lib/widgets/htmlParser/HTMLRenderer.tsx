28a5ce8bea712af15fe54bf9c73bd59bece495d4
muhametyanovti <muhametyanovti@sovcombank.ru>
2025-05-30 15:05:36 +0300

    fix: 🐛 fix html renderer

--- lib/widgets/htmlParser/HTMLRenderer.tsx
+++ lib/widgets/htmlParser/HTMLRenderer.tsx
@@ -1,7 +1,5 @@
 'use client'
-import { Fragment, useEffect, useState } from 'react'
 import type { ParserOptions } from './baseHtmlParser'
-import { ReactHTMLParser } from './reactHtmlParser'
 
 type ElementTag = keyof React.JSX.IntrinsicElements
 
@@ -11,35 +9,9 @@ type HTMLRendererProps<AS extends ElementTag, Props = AS extends ElementTag ? Re
   as?: AS
 } & Props
 
-type AsyncRenderProps = {
-  promises: Promise<React.ReactNode[]>
-}
+export const HTMLRenderer = <AS extends ElementTag>({ html, as, ...props }: HTMLRendererProps<AS>) => {
+  const Wrapper = as ?? 'div'
 
-const AsyncRender = ({ promises }: AsyncRenderProps) => {
-  const [nodes, setNodes] = useState<React.ReactNode[]>([])
-
-  useEffect(() => {
-    const resolve = async () => {
-      setNodes(await promises)
-    }
-
-    resolve()
-  }, [promises])
-
-  return <>{nodes}</>
-}
-
-export const HTMLRenderer = <AS extends ElementTag>({ html, options, as, ...props }: HTMLRendererProps<AS>) => {
-  const Wrapper = as ?? Fragment
-
-  const wrapperProps = as ? props : {}
-
-  const nodes = ReactHTMLParser.toReactNodes(html, options)
-
-  return (
-    // @ts-expect-error disable warning
-    <Wrapper {...wrapperProps}>
-      <AsyncRender promises={nodes} />
-    </Wrapper>
-  )
+  // @ts-expect-error abc
+  return <Wrapper dangerouslySetInnerHTML={{ __html: html }} {...props} />
 }
