0ef3da914c8afe8383376367bf3fa8399df1d3d9
galeevri <galeevri@sovcombank.ru>
2025-06-16 15:56:34 +0300

    feat: add current year for footer copyright

--- lib/widgets/footer/ui/Copyright.tsx
+++ lib/widgets/footer/ui/Copyright.tsx
@@ -10,10 +10,14 @@ interface CopyrightProps {
 }
 
 export const Copyright = ({ text, classes }: CopyrightProps) => {
+  const currentYear = new Date().getFullYear()
+
   return (
     <div
       className={cn('desk-body-regular-m text-color-footer', classes?.copyRight)}
-      dangerouslySetInnerHTML={{ __html: text }}
-    ></div>
+      dangerouslySetInnerHTML={{
+        __html: `<div style="display: inline-flex; gap: 4px"><span>© 2004-${currentYear}</span> ${text}</div>`
+      }}
+    />
   )
 }
