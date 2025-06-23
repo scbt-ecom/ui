d359ec5352544b341572afe5d2bb38aba9209c79
muhametyanovti <muhametyanovti@sovcombank.ru>
2025-06-23 14:43:53 +0300

    refactor: 💡 refactor session storage utils

--- lib/shared/utils/sessionStorage.ts
+++ lib/shared/utils/sessionStorage.ts
@@ -1,31 +1,37 @@
 import { TypeGuards } from './typeGuards'
 import { isClient } from '$/shared/utils/isClient'
 
-export const setSessionStorage = <T>(key: string, value: T) => {
-  if (!isClient) {
-    return console.warn('Session storage can be used only in client side')
-  }
+export const sessionStorageActions = {
+  setItem: <T>(key: string, value: T): void => {
+    if (!isClient) {
+      return console.warn('Session storage can be used only in client side')
+    }
 
-  if (TypeGuards.isNil(value)) return
+    if (TypeGuards.isNil(value)) return
 
-  const payload = JSON.stringify(value)
+    const payload = JSON.stringify(value)
 
-  sessionStorage.setItem(key, payload)
-}
+    sessionStorage.setItem(key, payload)
+  },
+  getItem: <T>(key: string): T | null => {
+    if (!isClient) {
+      console.warn('Session storage can be used only in client side')
+      return null
+    }
 
-export const getSessionStorage = <T>(key: string): T | null => {
-  if (!isClient) {
-    console.warn('Session storage can be used only in client side')
-    return null
-  }
+    const item = sessionStorage.getItem(key)
 
-  const item = sessionStorage.getItem(key)
+    if (TypeGuards.isNull(item)) return null
 
-  if (TypeGuards.isNull(item)) return null
-
-  try {
-    return JSON.parse(item) as T
-  } catch {
-    return item as T
+    try {
+      return JSON.parse(item) as T
+    } catch {
+      return item as T
+    }
+  },
+  clearItems: (...keys: string[]): void => {
+    keys.forEach((key) => {
+      sessionStorage.removeItem(key)
+    })
   }
 }
