e678b6b1081a08589ec3b84b212d9bd7c7351b64
muhametyanovti <muhametyanovti@sovcombank.ru>
2025-07-01 12:11:28 +0300

    refactor: 💡 migrate navigation logic into hook

--- lib/shared/ui/dropdownList/DropdownList.tsx
+++ lib/shared/ui/dropdownList/DropdownList.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useRef, useState } from 'react'
-import { isOptionActive, listNavigate } from './model'
+import { useRef } from 'react'
+import { useKeyboardNavigation } from './hooks'
+import { isOptionActive } from './model'
 import { DropdownItem, type DropdownItemOption } from './ui'
 import { useClickOutside } from '$/shared/hooks'
 import { cn } from '$/shared/utils'
@@ -35,73 +36,19 @@ export const DropdownList = <Multi extends boolean>({
   ...props
 }: DropdownListProps<Multi>) => {
   const ref = useRef<HTMLUListElement>(null)
-  const itemRefs = useRef<HTMLLIElement[]>([])
 
-  const [focusedIndex, setFocusedIndex] = useState<number>(-1)
+  const { refs, focusedIndex, setFocusedIndex } = useKeyboardNavigation<HTMLUListElement, HTMLLIElement>({
+    options,
+    multiple,
+    onPick
+  })
 
   useClickOutside(ref, () => setFocusedIndex(-1))
-
-  useEffect(() => {
-    if (!ref.current) return
-
-    const abortController = new AbortController()
-    const container = ref.current
-
-    window.addEventListener(
-      'keydown',
-      ({ key }) => {
-        let direction: 1 | -1 = -1
-
-        switch (key) {
-          case 'ArrowUp':
-            direction = -1
-            break
-          case 'ArrowDown':
-            direction = 1
-            break
-          case ' ':
-            if (focusedIndex < 0) return
-
-            const selectedItem = options[focusedIndex]
-            onPick?.(selectedItem)
-            if (!multiple) setFocusedIndex(-1)
-
-            return
-          default:
-            return
-        }
-
-        let currentIndex = listNavigate(focusedIndex, direction, options.length)
-
-        while (options[currentIndex].disabled) {
-          currentIndex = listNavigate(currentIndex, direction, options.length)
-        }
-
-        const scrollElement = itemRefs.current[currentIndex]
-
-        if (scrollElement) {
-          const scrollTop = scrollElement.offsetTop - container.offsetTop - 4
-
-          container.scrollTo({
-            top: scrollTop,
-            behavior: 'smooth'
-          })
-        }
-
-        setFocusedIndex(currentIndex)
-      },
-      { signal: abortController.signal }
-    )
-
-    return () => {
-      abortController.abort()
-    }
-  }, [focusedIndex, options])
 
   return (
     <ul
       {...props}
-      ref={ref}
+      ref={refs.setRoot()}
       className={cn(
         'customScrollbar-y z-10 mt-1 max-h-[264px] w-full overflow-y-auto scroll-smooth rounded-md bg-color-white p-1 shadow-[0_8px_20px_0px_rgba(41,41,41,0.08)]',
         className
@@ -112,11 +59,7 @@ export const DropdownList = <Multi extends boolean>({
 
         return (
           <DropdownItem
-            ref={(node) => {
-              if (!node) return
-
-              itemRefs.current.push(node)
-            }}
+            ref={refs.setReference}
             key={index}
             item={option}
             active={active}
---
+++ lib/shared/ui/dropdownList/hooks/useKeyboardNavigation.ts
@@ -0,0 +1,92 @@
+import { useEffect, useMemo, useRef, useState } from 'react'
+import { listNavigate } from '../model'
+import { type DropdownItemOption } from '../ui/dropdownItem'
+
+export type UseKeyboardNavigationProps = {
+  options: DropdownItemOption[]
+  multiple?: boolean
+  onPick?: (option: DropdownItemOption) => void
+}
+
+export const useKeyboardNavigation = <Container extends HTMLElement, Element extends HTMLElement>({
+  options,
+  multiple,
+  onPick
+}: UseKeyboardNavigationProps) => {
+  const [focusedIndex, setFocusedIndex] = useState<number>(-1)
+
+  const containerRef = useRef<Container>(null)
+  const itemRefs = useRef<Element[]>([])
+
+  const refs = useMemo(
+    () => ({
+      setReference: (node: Element | null) => {
+        if (node) itemRefs.current.push(node)
+      },
+      setRoot: () => containerRef
+    }),
+    []
+  )
+
+  useEffect(() => {
+    if (!containerRef.current) return
+
+    const abortController = new AbortController()
+    const container = containerRef.current
+
+    window.addEventListener(
+      'keydown',
+      ({ key }) => {
+        let direction: 1 | -1 = -1
+
+        switch (key) {
+          case 'ArrowUp':
+            direction = -1
+            break
+          case 'ArrowDown':
+            direction = 1
+            break
+          case ' ':
+            if (focusedIndex < 0) return
+
+            const selectedItem = options[focusedIndex]
+            onPick?.(selectedItem)
+            if (!multiple) setFocusedIndex(-1)
+
+            return
+          default:
+            return
+        }
+
+        let currentIndex = listNavigate(focusedIndex, direction, options.length)
+
+        while (options[currentIndex].disabled) {
+          currentIndex = listNavigate(currentIndex, direction, options.length)
+        }
+
+        const scrollElement = itemRefs.current[currentIndex]
+
+        const containerTop = container.scrollTop
+        const containerBottom = container.scrollTop + container.clientHeight
+
+        if (scrollElement.offsetTop < containerTop || scrollElement.offsetTop > containerBottom) {
+          const scrollTop = scrollElement.offsetTop - container.offsetTop - 4
+
+          container.scrollTo({
+            top: scrollTop,
+            behavior: 'smooth'
+          })
+        }
+
+        setFocusedIndex(currentIndex)
+      },
+      { signal: abortController.signal }
+    )
+
+    return () => {
+      abortController.abort()
+    }
+  }, [focusedIndex, options])
+
+  return { refs, focusedIndex, setFocusedIndex }
+}
