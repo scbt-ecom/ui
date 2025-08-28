2a4355aed2cad59ede70b52505b2124745ea5ed8
muhametyanovti <muhametyanovti@sovcombank.ru>
2025-08-28 14:44:35 +0300

    fix: 🐛 remove shit clsx lib

--- lib/shared/utils/cn.ts
+++ lib/shared/utils/cn.ts
@@ -1,6 +1,6 @@
-import clsx, { type ClassValue } from 'clsx'
+import clsx, { type ArgumentArray } from 'classnames'
 import { twMerge } from 'tailwind-merge'
 
-export const cn = (...inputs: ClassValue[]) => {
+export const cn = (...inputs: ArgumentArray) => {
   return twMerge(clsx(inputs))
 }
