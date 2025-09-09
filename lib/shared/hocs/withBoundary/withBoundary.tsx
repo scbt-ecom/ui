44636c97c0b718d53ee91c3d49eddb5772f33ae4
muhametyanovti <muhametyanovti@sovcombank.ru>
2025-09-09 12:28:22 +0300

    refactor: 💡 migrate error boundary into component

--- lib/shared/hocs/withBoundary/withBoundary.tsx
+++ lib/shared/hocs/withBoundary/withBoundary.tsx
@@ -1,37 +1,16 @@
-import { Component as ComponentClass, type ErrorInfo, type FC, type ReactElement } from 'react'
+import { type ComponentType, type JSX, type ReactElement } from 'react'
+import { ErrorBoundary } from '$/shared/utils'
 
 type BoundaryOptions = {
-  fallback?: boolean | ReactElement | string
+  fallback?: boolean | ReactElement | string | ((error: Error) => JSX.Element)
 }
 
-export const withBoundary = <P = {},>(Component: FC<P>, options?: BoundaryOptions) => {
-  return class extends ComponentClass<P, { hasError: boolean }> {
-    constructor(props: P) {
-      super(props)
-
-      this.state = {
-        hasError: false
-      }
-    }
-
-    static getDerivedStateFromError() {
-      return { hasError: true }
-    }
-
-    componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-      console.warn(error, errorInfo.componentStack)
-    }
-
-    getFallback() {
-      const { fallback = false } = options ?? {}
-
-      return fallback ?? null
-    }
-
-    render() {
-      if (this.state.hasError) return this.getFallback()
-
-      return <Component {...this.props} />
-    }
+export const withBoundary = <P extends {} = {}>(Render: ComponentType<P>, options?: BoundaryOptions) => {
+  return (props: P) => {
+    return (
+      <ErrorBoundary {...options}>
+        <Render {...props} />
+      </ErrorBoundary>
+    )
   }
 }
